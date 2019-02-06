865d7175e85a6597cf76e13bba556222271fae5c
ashutoshpith <amxdp18@gmail.com>
2019-02-07 02:51:44 +0530

    inital commit

---
+++ cmake-build-debug/CMakeFiles/FileMan.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileMan.dir/main.cpp.obj"
+  "FileMan.pdb"
+  "FileMan.exe"
+  "FileMan.exe.manifest"
+  "libFileMan.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileMan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
